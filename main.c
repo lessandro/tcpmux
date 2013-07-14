3d1cf6ae2c53faa133efb6807c12d8280717ae24
Lessandro Mariano <lessandro@gmail.com>
2013-07-14 13:24:27 -0300

    Add basic socket handling code

--- main.c
+++ main.c
@@ -1,50 +1,88 @@
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
 #include <ev.h>
 #include "redismq/redismq.h"
 
-#define REDIS_HOST "127.0.0.1"
-#define REDIS_PORT 6379
-#define REDIS_DB 0
-#define KEY "list"
+#define ASSERT(cond, name) do { \
+    if (!(cond)) { \
+        perror(name); \
+        exit(-1); \
+    } \
+} while (0);
 
-static int n = 0;
-
-static void timer_cb(EV_P_ struct ev_timer *timer, int revents)
+static int create_server(int port)
 {
-    char str[10];
-    sprintf(str, "test %d", n++);
+    int sd = socket(PF_INET, SOCK_STREAM, 0);
+    ASSERT(sd != -1, "socket");
 
-    printf("sending %s\n", str);
-    rmq_rpush(timer->data, str);
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    ASSERT(bind(sd, (struct sockaddr*)&addr, sizeof(addr)) != -1, "bind");
+    ASSERT(listen(sd, 2) != -1, "listen");
+
+    return sd;
 }
 
-static void blpop_cb(char *msg)
+void read_cb(struct ev_loop *loop, struct ev_io *watcher, int revents)
 {
-    printf("received: %s\n", msg);
+    ASSERT((revents & EV_ERROR) == 0, "revents");
+
+    char buffer[1024];
+
+    // Receive message from client socket
+    ssize_t read = recv(watcher->fd, buffer, 1023, 0);
+
+    if (read < 0) {
+        perror("recv");
+        return;
+    }
+
+    if (read == 0) {
+        printf("disconnected\n");
+
+        ev_io_stop(EV_DEFAULT_ watcher);
+        free(watcher);
+        return;
+    }
+
+    buffer[read] = '\0';
+    printf("message: %s\n", buffer);
+
+    send(watcher->fd, buffer, read, 0);
+}
+
+void accept_cb(struct ev_loop *loop, struct ev_io *watcher, int revents)
+{
+    ASSERT((revents & EV_ERROR) == 0, "revents");
+
+    struct sockaddr_in addr;
+    socklen_t addr_len = sizeof(addr);
+
+    int sd = accept(watcher->fd, (struct sockaddr*)&addr, &addr_len);
+    ASSERT(sd != -1, "accept");
+
+    printf("connected\n");
+
+    struct ev_io *w_client = (struct ev_io*)malloc(sizeof(struct ev_io));
+    ev_io_init(w_client, read_cb, sd, EV_READ);
+    ev_io_start(EV_DEFAULT_ w_client);
 }
 
 int main(int argc, char *argv[])
 {
     signal(SIGPIPE, SIG_IGN);
 
-    // rpush timer
+    int sd = create_server(5555);
 
-    struct rmq_context push;
-    rmq_init(&push, REDIS_HOST, REDIS_PORT, REDIS_DB, KEY);
+    struct ev_io w_accept;
 
-    struct ev_timer timer;
-    ev_timer_init(&timer, timer_cb, 0., .4);
-    timer.data = &push;
-    ev_timer_start(EV_DEFAULT_ &timer);
-
-    // blpop handler
-
-    struct rmq_context pop;
-    rmq_init(&pop, REDIS_HOST, REDIS_PORT, REDIS_DB, KEY);
-    rmq_blpop(&pop, blpop_cb);
-
-    // start libev event loop
+    ev_io_init(&w_accept, accept_cb, sd, EV_READ);
+    ev_io_start(EV_DEFAULT_ &w_accept);
 
     ev_loop(EV_DEFAULT_ 0);
 
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 all: tcpmux
 
-tcpmux: redismq/redismq.a
-	$(CC) -o tcpmux *.c redismq/redismq.a -lev -lhiredis
+tcpmux: redismq/redismq.a *.c
+	$(CC) -Wall -o tcpmux *.c redismq/redismq.a -lev -lhiredis
 
 redismq/redismq.a:
 	$(MAKE) -C redismq static-lib
