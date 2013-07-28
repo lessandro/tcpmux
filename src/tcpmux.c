2b00fe6c3ce740026d0c8d172856cbd2f6f3368d
Lessandro Mariano <lessandro@gmail.com>
2013-07-28 17:46:00 -0300

    Rewrite tcpmux.c with mux.h

--- src/tcpmux.c
+++ src/tcpmux.c
@@ -23,177 +23,53 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-// for asprintf / strdup
-#define _GNU_SOURCE 1
-
-#include <signal.h>
 #include <stdio.h>
 #include <string.h>
-#include <ev.h>
-#include <sys/queue.h>
-#include "tree.h"
-#include "../redismq/redismq.h"
 #include "../sev/sev.h"
-
-#define REDIS_HOST "127.0.0.1"
-#define REDIS_PORT 6379
-#define REDIS_DB 7
+#include "mux.h"
 
 #define PORT 5555
-#define NAME "tcpmux"
-#define MQ_OUT "kernel"
 
-#define BUFFER_SIZE 1024
+char *name = "tcpmux";
 
-struct rmq_context mq_out, mq_in;
-struct sev_server server;
-
-struct tm_client {
-    char *tag;
-    struct sev_stream *stream;
-
-    char buffer[BUFFER_SIZE];
-    size_t buffer_len;
-    int buffer_start;
-
-    RB_ENTRY(tm_client) entry;
-};
-
-int tm_client_cmp(struct tm_client *e1, struct tm_client *e2)
+void server_message(struct mux_client *client, char *message)
 {
-    return strcmp(e1->tag, e2->tag);
+    size_t len = strlen(message);
+
+    // send data
+    sev_send(client->stream, message, len);
 }
 
-RB_HEAD(tm_client_tree, tm_client) head = RB_INITIALIZER(&head);
-RB_GENERATE(tm_client_tree, tm_client, entry, tm_client_cmp);
-
-struct tm_client *tm_client_new(struct sev_stream *stream)
+void server_close(struct mux_client *client)
 {
-    struct tm_client *client = malloc(sizeof(struct tm_client));
-
-    asprintf(&client->tag, "%s:%s-%d", NAME, stream->remote_address,
-        stream->remote_port);
-    client->stream = stream;
-    sprintf(client->buffer, "message %s ", client->tag);
-    client->buffer_len = strlen(client->buffer);
-    client->buffer_start = client->buffer_len;
-
-    RB_INSERT(tm_client_tree, &head, client);
-
-    return client;
+    sev_close(client->stream, "server_close");
 }
 
-void tm_client_free(struct tm_client *client)
+static void open_cb(struct sev_stream *stream)
 {
-    RB_REMOVE(tm_client_tree, &head, client);
-
-    free(client->tag);
-    free(client);
-}
-
-void open_cb(struct sev_stream *stream)
-{
-    printf("open %s:%d\n", stream->remote_address, stream->remote_port);
-
-    struct tm_client *client = tm_client_new(stream);
+    struct mux_client *client = mux_client_open(stream);
     stream->data = client;
-
-    rmq_rpushf(&mq_out, "connect %s %s", client->tag, stream->remote_address);
 }
 
-int find_delimiter(const char *data, size_t len)
+static void read_cb(struct sev_stream *stream, char *data, size_t len)
 {
-    for (int i = 0; i < len; i++)
-        if (data[i] == '\n')
-            return i;
+    struct mux_client *client = stream->data;
 
-    return -1;
+    mux_client_data(client, data, len);
 }
 
-void read_cb(struct sev_stream *stream, char *data, size_t len)
+static void close_cb(struct sev_stream *stream, const char *reason)
 {
-    struct tm_client *client = stream->data;
+    struct mux_client *client = stream->data;
 
-    while (len != 0) {
-        int pos = find_delimiter(data, len);
-
-        int n = pos == -1 ? len : pos;
-
-        int rest = (BUFFER_SIZE - 1) - client->buffer_len;
-        if (n > rest)
-            n = rest;
-
-        // append data (up to \n) to the buffer
-        memcpy(client->buffer + client->buffer_len, data, n);
-        client->buffer_len += n;
-        client->buffer[client->buffer_len] = '\0';
-
-        // did not find a \n -- wait for the rest of the message
-        if (pos == -1)
-            return;
-
-        // terminate string on the first \r
-        char *p = strchr(client->buffer, '\r');
-        if (p)
-            *p = '\0';
-
-        rmq_rpush(&mq_out, client->buffer);
-        client->buffer_len = client->buffer_start;
-
-        // discard data before the delimiter, and the delimiter
-        data += pos + 1;
-        len -= pos + 1;
-    }
+    mux_client_close(client, reason);
 }
 
-void close_cb(struct sev_stream *stream)
+int main(int argc, char *argv[])
 {
-    printf("close %s:%d\n", stream->remote_address, stream->remote_port);
+    mux_init();
 
-    struct tm_client *client = stream->data;
-    rmq_rpushf(&mq_out, "disconnect %s %s", client->tag, "reason");
-    tm_client_free(client);
-}
-
-void blpop_cb(const char *reply)
-{
-    char *tags = strdup(reply);
-    char *message = strchr(tags, ' ');
-    *message++ = '\0';
-
-    char *tag = strtok(tags, ",");
-    for (; tag != NULL; tag = strtok(NULL, ",")) {
-        struct tm_client key = { .tag = tag };
-        struct tm_client *client = RB_FIND(tm_client_tree, &head, &key);
-
-        if (client == NULL)
-            continue;
-
-        if (!*message) {
-            sev_close(client->stream);
-            continue;
-        }
-
-        if (sev_send(client->stream, message, strlen(message)) == -1) {
-            sev_close(client->stream);
-            continue;
-        }
-    }
-
-    free(tags);
-}
-
-void mq_init(void)
-{
-    rmq_init(&mq_out, REDIS_HOST, REDIS_PORT, REDIS_DB, "mq:" MQ_OUT);
-    rmq_rpush(&mq_out, "reset " NAME " server restart");
-
-    rmq_init(&mq_in, REDIS_HOST, REDIS_PORT, REDIS_DB, "mq:" NAME);
-    rmq_blpop(&mq_in, blpop_cb);
-}
-
-int sev_init(void)
-{
+    struct sev_server server;
     if (sev_listen(&server, PORT)) {
         perror("sev_server_init");
         return -1;
@@ -203,18 +79,9 @@ int sev_init(void)
     server.read_cb = read_cb;
     server.close_cb = close_cb;
 
-    return 0;
-}
+    printf("%s started on port %d\n", name, PORT);
 
-int main(int argc, char *argv[])
-{
-    signal(SIGPIPE, SIG_IGN);
-
-    mq_init();
-    if (sev_init() == -1)
-        return -1;
-
-    ev_loop(EV_DEFAULT_ 0);
+    sev_loop();
 
     return 0;
 }
